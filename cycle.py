c625bf3216e580e66d2ed48649caf2ff312f2736
Jorge Martinez <jorgem13@mit.edu>
2025-02-04 13:46:33 -0500

    Added second computation and report of cycles prioritizing schedule conflicts

--- cycle.py
+++ cycle.py
@@ -15,8 +15,14 @@ def data_process(file_path):
     df["Timestamp"] = pd.to_datetime(df["Timestamp"], errors="coerce")
     df = df.dropna(subset=["Timestamp"])
     df_filtered = df.loc[df.groupby("Kerb (without @mit.edu)")["Timestamp"].idxmax()]
+
+
+    sc_students = set()
+    for _, row in df_filtered.iterrows():
+        if row["Reason for Switching (if your case doesn't fit into these options, email 6.1800-utas@mit.edu with your specific case)"].strip() == "Schedule conflict":
+            sc_students.add(row["Name"].strip())
     
-    return df_filtered
+    return df_filtered, sc_students
 
 
 def df_to_graph(df):
@@ -32,18 +38,26 @@ def df_to_graph(df):
     section_to_students = {}
     for _, row in df.iterrows():
         student = row["Name"].strip()
-        current_section = (row["Which recitation section are you currently in?"].strip(), row["Which tutorial section are you currently in?"].strip())
+        current_section = (
+            row["Which recitation section are you currently in?"].strip(), 
+            row["Which tutorial section are you currently in?"].strip()
+        )
         section_to_students.setdefault(current_section, []).append(student.strip())
 
     # Add edges to others currently in desired sections
     for _, row in df.iterrows():
 
         student = row["Name"].strip()
-        current_section = (row["Which recitation section are you currently in?"].strip(), row["Which tutorial section are you currently in?"].strip())
+        current_section = (
+            row["Which recitation section are you currently in?"].strip(), 
+            row["Which tutorial section are you currently in?"].strip()
+        )
 
         desired_recitation_sections = str(row["Which recitation sections would you like?"]).split(", ")
         desired_tutorial_sections = str(row["Which tutorial section would you like?"]).split(", ")
 
+        # `desired_sections` includes sections with incompatible rec+tut instructors, but 
+        # `section_to_students` only has valid sections, so this shouldn't be an issue
         desired_sections = []
         for drs in desired_recitation_sections:
             for dts in desired_tutorial_sections:
@@ -63,7 +77,7 @@ def find_cycles(G):
     cycles = list(nx.simple_cycles(G))
     return sorted([list(cycle) for cycle in cycles], key=len, reverse=True)
 
-def optimal_removal(G):
+def optimal_removal(G, sc_students=None):
     """
     Iteratively remove cycles and see which selection of cycles results in the most students being satisfied
     """
@@ -77,27 +91,60 @@ def optimal_removal(G):
         G_c = G.copy()
         for node in cycle:
             G_c.remove_node(node)
-        cycle_groups, count = optimal_removal(G_c)
-        if len(cycle) + count > max_count:
+        cycle_groups, count = optimal_removal(G_c, sc_students)
+        satisfied = len(cycle) if sc_students is None else len((set(cycle) & sc_students))
+        if satisfied + count > max_count:
             max_cycle_groups = cycle_groups + [cycle]
-            max_count = len(cycle) + count
+            max_count = satisfied + count
     return max_cycle_groups, max_count
 
 def main():
     file_path = "./form_data.csv"
-    df = data_process(file_path)
+    df, sc_students = data_process(file_path)
     num_students = df.shape[0]
+    num_sc_students = len(sc_students)
     G = df_to_graph(df)
+
+    # Overall
     swap_chains, count = optimal_removal(G)
+    overall_fix = round(count/num_students*100, 2)
+    sc_students_fixed = set()
+    for chain in swap_chains:
+        sc_students_fixed |= (sc_students & set(chain))
+    sc_students_fixed = len(sc_students_fixed)
+    sc_students_fixed = round(sc_students_fixed/num_sc_students*100, 2)
 
     if swap_chains:
-        print(f"Best free swaps ({round(count/num_students*100, 2)}% fix rate):")
+        print(f"\nBest free swaps ({overall_fix}% of appeals fixed ; {sc_students_fixed}% of schedule conflicts fixed):")
         for i, swap_chain in enumerate(reversed(swap_chains)):
             names = [name for name, _ in swap_chain]
-            print(f"{i+1}. {" -> ".join(names)}")
-        print("Remember to manually fix schedule conflicts and confirm special cases/exceptions.")
+            print(f"\t{i+1}. {" -> ".join(names)}")
     else:
-        print("No easy swaps found. Rip.")
+        print("No easy swaps found in general. Rip.\n")
+
+    # Schedule conflicts
+    sc_swap_chains, sc_count = optimal_removal(G, sc_students)
+    sc_names = set()
+    for chain in sc_swap_chains:
+        sc_names |= set(chain)
+    removed_nodes = [node for node in list(G.nodes) if node[0] in sc_names]
+    G.remove_nodes_from(removed_nodes)
+    swap_chains, extra = optimal_removal(G)
+    overall_count = sc_count + extra
+    overall_fix = round(overall_count/num_students*100, 2)
+    sc_fix = round(sc_count/num_sc_students*100, 2)
+    final_chain = sc_swap_chains + swap_chains
+
+    print("\n")
+    if final_chain:
+        print(f"Best swaps for schedule conflicts ({sc_fix}% of schedule conflicts fixed ; {overall_fix}% of appeals fixed):")
+        for i, sc_swap_chain in enumerate(reversed(final_chain)):
+            names = [name for name, _ in sc_swap_chain]
+            print(f"\t{i+1}. {" -> ".join(names)}")
+    else:
+        print("No easy swaps found for schedule conflicts. Rip.\n")
+    
+    print("\nRemember to manually fix schedule conflicts and confirm special cases/exceptions!\n")
 
 if __name__ == "__main__":
     main() 
